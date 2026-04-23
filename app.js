a1828c2ecaef996affdf0ac0e51251a56c480b5a
labib <labibul.ahsan.alif@g.bracu.ac.bd>
2026-04-23 21:46:41 +0600

    data fetch from openweather api

---
+++ app.js
@@ -0,0 +1,10 @@
+// const apiKey = "Your Api key is here"
+// const url =  `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`;
+
+// function getWeather(){
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
+//         })
+// }
